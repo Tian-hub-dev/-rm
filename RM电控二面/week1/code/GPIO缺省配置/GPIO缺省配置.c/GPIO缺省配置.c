7e816dbb59c7b8df4a57bd1f24c18e4397b24403
Tian-hub-dev <3674730582@qq.com>
2025-10-13 23:55:25 +0800

    Update GPIO缺省配置.c

--- RM电控二面/week1/code/GPIO缺省配置/GPIO缺省配置.c/GPIO缺省配置.c
+++ RM电控二面/week1/code/GPIO缺省配置/GPIO缺省配置.c/GPIO缺省配置.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 
-typedef enum { GPIO_Speed_2MHz, GPIO_Speed_10MHz, GPIO_Speed_50MHz } GPIO_Speed_MHz;
+typedef enum { GPIO_Speed_2MHz, GPIO_Speed_10MHz, GPIO_Speed_50MHz } GPIO_Speed_MHz;          //枚举的时候，我觉得这样看的更舒服，就不太符合规范
 
-typedef struct { GPIO_Speed_MHz GPIO_Speed; } GPIO_Speed_structure;
+typedef struct { GPIO_Speed_MHz GPIO_Speed; } GPIO_Speed_structure;                           // 同上
 
 void GPIO_StructureInit(GPIO_Speed_structure* pGPIO_Speed_struct)
 {
@@ -14,4 +14,5 @@ int main()
 	GPIO_Speed_structure  GPIO_Speed_struct;
 	GPIO_StructureInit(&GPIO_Speed_struct);
 	return 0;
+
 }
