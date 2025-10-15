c91e72d8b01241e433589ea8a16be341782a50dc
Tian-hub-dev <3674730582@qq.com>
2025-10-15 18:12:30 +0800

    Add files via upload

---
+++ RM电控二面/week1/code/hello world/循环.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int number;
+	do {
+		printf("«Î ‰»Î-1/0/1");
+		scanf_s("%d", &number);
+		if (number == 0) { printf("helloworld\\n"); }
+		else if (number == 1) { printf("HELLOWORLD\\n"); }
+	} while (number != -1);
+	return 0;
+}
---
+++ RM电控二面/week1/code/软件模拟IIC/I2C.h
@@ -0,0 +1,13 @@
+#ifndef I2C_h                             //∂®“Â∫Í√˚
+#define I2C_h 
+
+#include <stdint.h> 
+
+void I2C_Start(void);
+void I2C_Stop(void);
+void I2C_SendByte(uint8_t data);
+uint8_t I2C_ReceiveAck(void);
+
+void I2C_SendData(uint8_t data);
+#endif
+
